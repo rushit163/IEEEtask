80b2aa222425a6ee64bc62e75663207f80c66329
Rushit Patel <rushitpatel@Rushits-MacBook-Air.local>
2022-10-22 19:46:50 +0530

    password generated

--- my-app/src/components/PasswordGenerator.jsx
+++ my-app/src/components/PasswordGenerator.jsx
@@ -1,4 +1,5 @@
 import React, { Component, useState } from 'react'
+import { createPassword } from '../services/ceratePassword';
 
 let PasswordGenerator =()=> {
     let [state,setState] = useState({
@@ -23,41 +24,46 @@ let PasswordGenerator =()=> {
             [event.target.name] : event.target.checked
         })
     };
-
+    let submit = (e) => {
+        e.preventDefault();
+        let passwordObject = createPassword.getObjects(state);
+        let thePassword =  createPassword.generatePassword(passwordObject, state.passwordlength)
+        setState({...state, generatedPassword : thePassword});
+    }
     return (
         <div className="container mt-5">
             {/* <pre>{JSON.stringify(state)}</pre> */}
             <div className='text-light'>Password Generator</div>
             <div className='row'>
-                <form>
+                <form onSubmit={submit}>
                 <div className="col">
                     <div className="bg-dark p-2">
                     <div className="input-group">
                         <input value={state.generatedPassword} onChange = {updateInput} type="text" className="form-control bg-dark password text-light" placeholder="Random Password" name='generatedPassword'/>
-                        <button className='input-group-text password bg-dark' ><i class="fa fa-copy"></i></button>
+                        <button className='input-group-text password bg-dark' ><i className="fa fa-copy"></i></button>
                     </div>
                     </div>
                     <div className="bg-dark mt-2 p-2">
                         <div className="d-flex justify-content-between">
-                        <label for="customRange3" class="form-label text-light">Character Length</label>
+                        <label htmlFor="customRange3" className="form-label text-light">Character Length</label>
                         <span className='text-light'>{state.passwordlength}</span>
                         </div>
-                        <input  value={state.passwordlength} onChange = {updateInput} name='passwordlength' type="range" class="form-range" min="0" max="50" step="1" id="customRange3"></input>
-                    <div class="form-check">
-                        <input onChange={updateCheckBox} name='upper' class="form-check-input" type="checkbox" value=""/>
-                        <label class="form-check-label text-light" for="flexCheckDefault">Include UpperCase Characters</label>
+                        <input  value={state.passwordlength} onChange = {updateInput} name='passwordlength' type="range" className="form-range" min="0" max="50" step="1" id="customRange3"></input>
+                    <div className="form-check">
+                        <input onChange={updateCheckBox} name='upper' className="form-check-input" type="checkbox"/>
+                        <label className="form-check-label text-light" htmlFor="flexCheckDefault">Include UpperCase Characters</label>
                     </div>
-                    <div class="form-check">
-                        <input onChange={updateCheckBox} name='lower' class="form-check-input" type="checkbox" value=""/>
-                        <label class="form-check-label text-light" for="flexCheckDefault">Include UpperCase Characters</label>
+                    <div className="form-check">
+                        <input onChange={updateCheckBox} name='lower' className="form-check-input" type="checkbox"/>
+                        <label className="form-check-label text-light" htmlFor="flexCheckDefault">Include LowerCase Characters</label>
                     </div>
-                    <div class="form-check">
-                        <input  onChange={updateCheckBox} name='numbers' class="form-check-input" type="checkbox" value=""/>
-                        <label class="form-check-label text-light" for="flexCheckDefault">Include Numbers</label>
+                    <div className="form-check">
+                        <input  onChange={updateCheckBox} name='numbers' className="form-check-input" type="checkbox"/>
+                        <label className="form-check-label text-light" htmlFor="flexCheckDefault">Include Numbers</label>
                     </div>
-                    <div class="form-check">
-                        <input onChange={updateCheckBox} name='symbol' class="form-check-input" type="checkbox" value="" id="flexCheckDefault"/>
-                        <label class="form-check-label text-light" for="flexCheckDefault">Include Symbols</label>
+                    <div className="form-check">
+                        <input onChange={updateCheckBox} name='symbol' className="form-check-input" type="checkbox"/>
+                        <label className="form-check-label text-light" htmlFor="flexCheckDefault">Include Symbols</label>
                     </div>
                     </div>
                     <div className="bg-dark p-2">
---
+++ my-app/src/services/ceratePassword.js
@@ -0,0 +1,38 @@
+export class createPassword{
+    static getRandomLowerCase(){
+        return String.fromCharCode(Math.floor(Math.random()*26) + 97);
+    }
+    static getRandomUpperCase(){
+        return String.fromCharCode(Math.floor(Math.random()*26) + 65);  
+    }
+    static getRandomNumber(){
+        return String.fromCharCode(Math.floor(Math.random()*10) + 48);    
+    }
+    static getRandomSymbol(){
+        let symbols = `~,./;'[]-=<>?:"{}_+\|`
+        return symbols[Math.floor(Math.random()*symbols.length)];
+    }
+    static getObjects(state){
+        let passwordobj = {};
+        for(let key of Object.keys(state)){
+            if(typeof state[key] === 'boolean' && state[key]){
+                passwordobj = {
+                    ...passwordobj,
+                    [key] : state[key]
+                }
+            }
+        }
+        return passwordobj;
+    }
+    
+    static generatePassword(passwordobj, passwordLength){
+       let rnadomPassword = '';
+       for(let i=0; i<Number(passwordLength); i+= Object.keys(passwordobj).length){
+             if(passwordobj.lower) rnadomPassword += `${this.getRandomLowerCase()}`;
+             if(passwordobj.upper) rnadomPassword += `${this.getRandomUpperCase()}`;
+             if(passwordobj.symbol) rnadomPassword += `${this.getRandomSymbol()}`;
+             if(passwordobj.numbers) rnadomPassword += `${this.getRandomNumber()}`;
+         }
+         return rnadomPassword;
+     }
+}
